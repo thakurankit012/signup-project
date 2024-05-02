7e8ffc1993c43d7f18afeda5f3c92a29fc427b65
Ankit Thakur <157781047+thakurankit012@users.noreply.github.com>
2024-05-02 22:35:09 +0530

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
-// import React from 'react';
-// import './App.css';
-// import SignupForm from './components/SignupForm';
-// import PostListScreen from './components/PostListScreen';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SignupPage from './Screen/SignupPage';
+import UserDetails from './Screen/UserDetails';
+import LoginPage from './Screen/LoginPage';
+function App() {
+  const [isSignedUp, setIsSignedUp] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userDetails, setUserDetails] = useState({});
+  const [registrationFailed, setRegistrationFailed] = useState(false);
 
-// const App = () => {
-//   return (
-//     <div>
-     
-//       <SignupForm/>
-//       <PostListScreen />
-//     </div>
-//   );
-// };
+  const handleLogin = (details) => {
+    setUserDetails(details);
+    setIsLoggedIn(true);
+  };
+  const handleSignUpSuccess = (details) => {
+    setUserDetails(details);
+    setIsSignedUp(true);
+  };
+  const handleSignUpFailed = () => {
+    setRegistrationFailed(true);
+  };
+  return (
+    <div>
+      {isLoggedIn ? (
+        <UserDetails userDetails={userDetails} />
+      ) : (
+        <>
+          {isSignedUp && !registrationFailed ? (<LoginPage onLogin={handleLogin} />) : (<SignupPage onSignUpSuccess={handleSignUpSuccess} onSignUpFailed={handleSignUpFailed} />)}
+        </>
+      )}
+    </div>
+  );
+}
+export default App;
 
-// export default App;
